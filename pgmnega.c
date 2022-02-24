7286ee64d194219d71ecb76955fea2e76eb8bf82
AlexF99 <alexandreopf@gmail.com>
2022-02-24 13:55:15 -0300

    funcoes get_parametros e aloca_matriz

--- pgmnega.c
+++ pgmnega.c
@@ -4,53 +4,64 @@
 
 #define LINESIZE 1024
 
-int main(int argc, char *argv[])
+void get_parametros(char *filename, FILE **img, char *pixeltype, int *numlinhas, int *numcolunas, int *maxgray)
 {
-
-    FILE *img;
-    char pixeltype[2];
-    int i, l = 0;
-    char *numlinhas;
-    char *numcolunas;
-    char imgsize[LINESIZE + 1];
-    char line[LINESIZE + 1];
-
-    for (i = 0; i < argc; i++)
-        printf("argv[%d]: %s\n", i, argv[i]);
-
-    img = fopen("lena.pgm", "r");
+    *img = fopen(filename, "r");
     if (!img)
     {
         perror("erro ao abrir arquivo");
         exit(1);
     }
 
-    fgets(pixeltype, LINESIZE, img);
-    printf("pixeltype: %s", pixeltype);
+    fscanf(*img, "%s", pixeltype);
+    fscanf(*img, "%d", numlinhas);
+    fscanf(*img, "%d", numcolunas);
+    fscanf(*img, "%d", maxgray);
+}
 
-    fgets(imgsize, LINESIZE, img);
-    printf("imgsize: %s", imgsize);
+void aloca_matriz_img(FILE **img, int **matriz_img, int numlinhas, int numcolunas)
+{
+    int *pixel;
+    int i, j;
 
-   numlinhas = strtok(imgsize, "  ");
-   numcolunas = strtok(imgsize, "  ");
-   printf( "linhas: %s\n", numlinhas );
-   printf( "colunas: %s\n", numcolunas );
-   
-//    while( numlinhas != NULL ) {
-//       printf( "%s\n", numlinhas );
-    
-//       numlinhas = strtok(NULL, " ");
-//    }
-    
+    matriz_img = malloc(numlinhas * sizeof(int *));
 
-    while (!feof(img) && l < 10)
-    {
-        fgets(line, LINESIZE, img);
-        printf("%d: %s", l, line);
-        l++;
-    }
+    for (i = 0; i < numlinhas; i++)
+        matriz_img[i] = malloc(numcolunas * sizeof(int));
+
+    for (i = 0; i < numlinhas; i++)
+        for (j = 0; j < numcolunas; j++) {
+            fscanf(img, "%d", pixel);
+            matriz_img[i][j] = pixel;
+        }
+
+    for (i = 0; i < numlinhas; i++)
+        free(matriz_img[i]);
+    free(matriz_img);
+}
+
+int main(int argc, char *argv[])
+{
+
+    FILE *arquivo_pgm;
+    int **matriz_img;
+    char *pixeltype;
+    int i, j, l = 0;
+    int numlinhas, numcolunas, maxgray;
+    char line[LINESIZE + 1];
+
+    get_parametros("lena.pgm", &arquivo_pgm, pixeltype, &numlinhas, &numcolunas, &maxgray);
+
+    aloca_matriz_img(&arquivo_pgm, matriz_img, numlinhas, numcolunas);
+
+    // while (!feof(arquivo_pgm) && l < 10)
+    // {
+    //     fgets(line, LINESIZE, arquivo_pgm);
+    //     printf("%d: %s", l, line);
+    //     l++;
+    // }
 
     printf("\n\n");
-    fclose(img);
+    fclose(arquivo_pgm);
     return 0;
 }
